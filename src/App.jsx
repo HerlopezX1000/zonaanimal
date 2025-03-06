4db5b1f35bdbd90dfb28a5b82e5d2cfb11ded9f6
HerlopezX1000 <herlopez@gmail.com>
2025-03-06 17:11:10 -0500

    actualice el archivo Inicio.jsx a su real ubicacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-import Inicio from './components/pages/Inicio'; // Importa el componente Inicio
+import Inicio from "./components/pages/Inicio.jsx";
 
 function App() {
   return (
